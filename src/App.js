29534cc54682b7cebcffb70ba7172f91689c7c0b
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-16 20:26:11 +0000

    Refactor code to utilize a custom context hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
@@ -6,32 +5,11 @@ import { Container } from 'react-bootstrap';
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
 
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
-
   return (
     <Router>
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
       <div className={styles.App}>
         <NavBar />
         <Container className={styles.Main}>
@@ -46,8 +24,6 @@ function App() {
           </Switch>
         </Container>
       </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
     </Router>
   );
 }
