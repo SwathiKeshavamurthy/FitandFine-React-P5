c00988634499aa2e13544c40951fdc97f5e74e53
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-16 20:35:08 +0000

    Create axios interceptors axiosRes and axiosReq and and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://fitandfine-drf-be560b223a3b.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
