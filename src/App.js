9ddf134bca00666fbfdbc114c9f6ef3d5d9a92ce
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-16 10:44:06 +0000

    Routing NavBar in React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import styles from "./App.module.css";
-import NavBar from "./components/NavBar";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <NavBar />
-    </div>
+    <Router>
+      <div className={styles.App}>
+        <NavBar />
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home Page</h1>} />
+            <Route path="/challenges" render={() => <h1>Challenges Page</h1>} />
+            <Route path="/collaborate" render={() => <h1>About & Collaborate</h1>} />
+            <Route path="/signin" render={() => <h1>Sign In</h1>} />
+            <Route path="/signup" render={() => <h1>Sign Up</h1>} />
+            <Route component={() => <p>Page not found!</p>} />
+            
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,59 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { Helmet } from 'react-helmet';
 import logo from "../assets/logo.JPG";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
-      <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="100" />
-          <Helmet>
-            <title>Fit&Fine</title>
-          </Helmet>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
-            <Nav.Link>
-              <i className="fas fa-home-user"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-trophy"></i>Challenges
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-circle-info"></i>About&Collaborate
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-right-to-bracket"></i>Login
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fas fa-user-plus"></i>Register
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Container>
+          <NavLink to="/" className={styles.NavBarBrand}>
+            <img src={logo} alt="Fit&Fine Logo" height="100" />
+          </NavLink>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto text-left">
+              <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/"
+              >
+                <i className="fas fa-home-user"></i> Home
+              </NavLink>
+              <NavLink
+                to="/challenges"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+              >
+                <i className="fas fa-trophy"></i> Challenges
+              </NavLink>
+              <NavLink
+                to="/collaborate"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+              >
+                <i className="fas fa-circle-info"></i> About & Collaborate
+              </NavLink>
+              <NavLink
+                to="/signin"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+              >
+                <i className="fas fa-right-to-bracket"></i> Login
+              </NavLink>
+              <NavLink
+                to="/signup"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+              >
+                <i className="fas fa-user-plus"></i> Register
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   );
 };
 
