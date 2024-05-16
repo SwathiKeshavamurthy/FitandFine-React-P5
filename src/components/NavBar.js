aa7f9ccbb4939d6373ab932971b94a9ed7511dae
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-16 20:13:00 +0000

    Use useContext to reflect the currently logged user

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.JPG";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
+
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
       <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
+
           <NavLink to="/" className={styles.NavBarBrand}>
             <img src={logo} alt="Fit&Fine Logo" height="100" />
+            <div>
+            <h1 className={styles.BrandText}>Fit&Fine</h1>
+            <h3 className={styles.BrandTagline}>Set. Sweat. Share. Shine</h3>
+            </div>
           </NavLink>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
@@ -22,6 +52,7 @@ const NavBar = () => {
               >
                 <i className="fas fa-home-user"></i> Home
               </NavLink>
+
               <NavLink
                 to="/challenges"
                 className={styles.NavLink}
@@ -29,6 +60,7 @@ const NavBar = () => {
               >
                 <i className="fas fa-trophy"></i> Challenges
               </NavLink>
+              
               <NavLink
                 to="/collaborate"
                 className={styles.NavLink}
@@ -36,20 +68,8 @@ const NavBar = () => {
               >
                 <i className="fas fa-circle-info"></i> About & Collaborate
               </NavLink>
-              <NavLink
-                to="/signin"
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-              >
-                <i className="fas fa-right-to-bracket"></i> Login
-              </NavLink>
-              <NavLink
-                to="/signup"
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-              >
-                <i className="fas fa-user-plus"></i> Register
-              </NavLink>
+              
+              {currentUser ? loggedInIcons : loggedOutIcons}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios"
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,8 +13,11 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
+
 
 function SignInForm() {
+    const setCurrentUser = useContext(SetCurrentUserContext);
     const [signInData, setSignInData] = useState({
       username: "",
       password: "",
@@ -27,12 +30,14 @@ function SignInForm() {
     const handleSubmit = async (event) => {
       event.preventDefault();
       try {
-        await axios.post("/dj-rest-auth/login/", signInData);
+        const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+        setCurrentUser(data.user);
         history.push("/");
       } catch (err) {
         setErrors(err.response?.data);
       }
     };
+  
     const handleChange = (event) => {
       setSignInData({
         ...signInData,
@@ -58,10 +63,10 @@ function SignInForm() {
                     />
                 </Form.Group>
                 {errors.username?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
+                 <Alert key={idx} variant="warning">
+                   {message}
+                 </Alert>
+               ))}
 
                 <Form.Group controlId="password">
                     <Form.Label className="d-none">Password</Form.Label>
@@ -89,12 +94,12 @@ function SignInForm() {
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
-            ))}git ad
+             ))}
             </Form>
             </Container>
             <Container className={`mt-3 ${appStyles.Content}`}>
               <Link className={styles.Link} to="/signup">
-                Don't have an account? <span>Sign up now!</span>
+                Don't have an account? <span>Register/Sign up now!</span>
               </Link>
             </Container>
           </Col>
