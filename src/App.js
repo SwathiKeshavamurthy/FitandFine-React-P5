f314bad913825499c983c4fff36e95354233fdb2
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-17 14:24:50 +0000

    Add feature to Edit and Delete PostEditForm page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostEditForm from "./pages/posts/PostEditForm"; // Import PostEditForm
 import PostPage from "./pages/posts/PostPage";
-import PostsPage from "./pages/posts/PostsPage";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostsPage from "./pages/posts/PostsPage"; // Import PostsPage
+import { useCurrentUser } from "./contexts/CurrentUserContext"; // Import useCurrentUser
 
 function App() {
   const currentUser = useCurrentUser();
@@ -51,6 +52,7 @@ function App() {
             <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route exact path="/posts/postcreate" render={() => <PostCreateForm />} />
             <Route exact path="/posts/:id" render={() => <PostPage />} />
+            <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
             <Route path="/challenges" render={() => <h1>Challenges Page</h1>} />
             <Route path="/collaborate" render={() => <h1>About & Collaborate</h1>} />
             <Route render={() => <p>Page not found!</p>} />
