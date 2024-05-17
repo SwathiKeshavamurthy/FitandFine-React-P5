ac548cbac352b3f99c2dd89068b967c787f95ba6
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-17 11:40:39 +0000

    Add filtering styles for post images and update CSS syntax

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
-import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
+import appStyles from "../../App.module.css";
 
 function PostPage() {
   const { id } = useParams();
@@ -15,9 +14,7 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
-          axiosReq.get(`/posts/${id}`),
-        ]);
+        const { data: post } = await axiosReq.get(`/posts/${id}`);
         setPost({ results: [post] });
         console.log(post);
       } catch (err) {
@@ -32,7 +29,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        {post.results.length > 0 && <Post {...post.results[0]} setPosts={setPost} postPage />}
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
