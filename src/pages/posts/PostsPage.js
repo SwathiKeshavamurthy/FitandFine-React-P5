f56286ff0662049f1ff12bdd5d21e334f3b56795
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-17 21:31:08 +0000

    Add the PopularProfiles component to PostsPage component and Responsiveness

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/noresults.JPG";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles"; // Import PopularProfiles
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -43,7 +44,7 @@ function PostsPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
+        <PopularProfiles mobile /> {/* Added PopularProfiles for mobile */}
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
@@ -83,7 +84,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles /> {/* Added PopularProfiles for desktop */}
       </Col>
     </Row>
   );
