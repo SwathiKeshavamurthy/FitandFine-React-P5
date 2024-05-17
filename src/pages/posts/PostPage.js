c209c2a177d7071b56b180c30bc747fb8884aa83
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-17 21:21:31 +0000

    Add the PopularProfiles component to PostPage component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,21 +1,18 @@
 import React, { useEffect, useState } from "react";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Comment from "../comments/Comment";
-
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostPage() {
   const { id } = useParams();
@@ -46,7 +43,9 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} postPage />
+        {post.results.length > 0 && (
+          <Post {...post.results[0]} setPosts={setPost} postPage />
+        )}
         <Container className={appStyles.Content}>
           {currentUser ? (
             <CommentCreateForm
@@ -82,7 +81,7 @@ function PostPage() {
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
