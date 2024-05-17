39bb3b52133a03eee4051f206e60f7ac0e0677e4
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-17 10:56:48 +0000

    Add Event hanler for post create form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
 
-import Upload from "../../assets/uplaod.jpg";
+import Asset from "../../components/Asset";
+
+import Upload from "../../assets/upload.jpg";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+const TAG_CHOICES = [
+  { value: 'cycling', label: 'Cycling' },
+  { value: 'hiking', label: 'Hiking' },
+  { value: 'swimming', label: 'Swimming' },
+  { value: 'yoga', label: 'Yoga' },
+  { value: 'running', label: 'Running' },
+  { value: 'physical_activity', label: 'Physical Activity' },
+  { value: 'nature', label: 'Nature' },
+  { value: 'other_activities', label: 'Other Activities' },
+];
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
@@ -21,8 +37,12 @@ function PostCreateForm() {
     title: "",
     content: "",
     image: "",
+    tags: "physical_activity", // Default tag
   });
-  const { title, content, image } = postData;
+  const { title, content, image, tags } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const handleChange = (event) => {
     setPostData({
@@ -41,6 +61,26 @@ function PostCreateForm() {
     }
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", imageInput.current.files[0]);
+    formData.append("tags", tags);
+
+    try {
+      const { data } = await axiosReq.post("/posts/", formData);
+      history.push(`/posts/${data.id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
+
   const textFields = (
     <div className="text-center">
       <Form.Group>
@@ -52,6 +92,12 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
       <Form.Group>
         <Form.Label>Content</Form.Label>
         <Form.Control
@@ -62,10 +108,36 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Tags</Form.Label>
+        <Form.Control
+          as="select"
+          name="tags"
+          value={tags}
+          onChange={handleChange}
+        >
+          {TAG_CHOICES.map((tag) => (
+            <option key={tag.value} value={tag.value}>
+              {tag.label}
+            </option>
+          ))}
+        </Form.Control>
+      </Form.Group>
+      {errors?.tags?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
@@ -76,7 +148,7 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -113,8 +185,22 @@ function PostCreateForm() {
                 id="image-upload"
                 accept="image/*"
                 onChange={handleChangeImage}
+                ref={imageInput}
+                style={{ display: "none" }}
               />
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.Blue} mt-2`}
+                onClick={() => imageInput.current.click()}
+              >
+                Upload Image
+              </Button>
             </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
