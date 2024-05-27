813361366c4a5d2dd699aee4c215aaafd008007d
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-27 20:38:59 +0000

    Create axios instances for request and response

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,36 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://fitandfine-drf-be560b223a3b.herokuapp.com/";
+// Set the base URL for all axios requests
+const BASE_URL = "https://fitandfine-drf-be560b223a3b.herokuapp.com/";
+
+// Default axios instance configuration
+axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+// Create axios instances for request and response
+export const axiosReq = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const axiosRes = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    axiosReq.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    axiosRes.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axiosReq.defaults.headers.common["Authorization"];
+    delete axiosRes.defaults.headers.common["Authorization"];
+  }
+};
